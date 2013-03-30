5f8352e5fa2e2aebb3c020a1a50745a8613c33f6
Fabrice Matrat <fabricematrat@gmail.com>
2013-03-30 15:30:24 +0100

    add mongo plugin

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -40,14 +40,13 @@ grails.project.dependency.resolution = {
     }
 
     plugins {
-        runtime ":hibernate:$grailsVersion"
         runtime ":jquery:1.9.1"
-        runtime ":resources:1.1.6"
+//        runtime ":resources:1.1.6"
         build ":tomcat:$grailsVersion"
-        runtime ":database-migration:1.1"
-        compile ':cache:1.0.0'
+//        compile ':cache:1.0.0'
         // http://support.cloudfoundry.com/entries/21014643-Grails-Spring-Security-deployment-problem
         // to fix cf/springsecurity issue
         compile ":webxml:1.4.1"
+        compile ":mongodb:1.2.0"
     }
 }
