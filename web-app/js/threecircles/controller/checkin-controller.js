17dc56a9bb8d8f1bbcbfa2b30c075a2fc9f40c28
corinnekrych <corinnekrych@gmail.com>
2013-04-03 01:30:26 +0200

    confess

--- web-app/js/threecircles/controller/checkin-controller.js
+++ web-app/js/threecircles/controller/checkin-controller.js
@@ -1,11 +1,10 @@
 var threecircles = threecircles || {};
 threecircles.controller = threecircles.controller || {};
 
-threecircles.controller.checkincontroller = function(feed, model, view) {
-    var that = grails.mobile.mvc.controller(feed, model, view);
+threecircles.controller.checkincontroller = function (feed, model, view, cfg) {
+    var that = grails.mobile.mvc.controller(feed, model, view, cfg);
 
-    //var baseURL = "http://localhost:8080/ThreeCircles/Checkin/";
-    var baseURL = "http://ThreeCircles.cloudfoundry.com/Checkin/";
+    var baseURL = cfg.baseURL;
 
     //-----------------------------------------------------------------------------
     // TODO attached a bahavior when loginButtonClicked is raised
