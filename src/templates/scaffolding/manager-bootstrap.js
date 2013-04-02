c9607ce7ead4b8960ce3a7d24fe58dc8b68afba2
corinnekrych <corinnekrych@gmail.com>
2013-04-03 01:24:32 +0200

    confess

--- src/templates/scaffolding/manager-bootstrap.js
+++
@@ -1,9 +0,0 @@
-<%
-projectName = project.toLowerCase()
-%>var ${projectName} = ${projectName} || {};
-
-${projectName}.load = (function () {
-    var managerObject = grails.mobile.mvc.manager(${projectName}.configuration);
-
-}());
-
