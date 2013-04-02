d40269f3778342b839e200fa5a193ff32ccdf668
corinnekrych <corinnekrych@gmail.com>
2013-04-03 01:03:52 +0200

    confess

--- web-app/js/threecircles/controller/user-controller.js
+++ web-app/js/threecircles/controller/user-controller.js
@@ -1,8 +1,8 @@
 var threecircles = threecircles || {};
 threecircles.controller = threecircles.controller || {};
 
-threecircles.controller.usercontroller = function (feed, model, view) {
-    var that = grails.mobile.mvc.controller(feed, model, view);
+threecircles.controller.usercontroller = function (feed, model, view, cfg) {
+    var that = grails.mobile.mvc.controller(feed, model, view, cfg);
 
     //Place here your custom event
 //    view.somethingButtonClicked.attach(function (item, context) {
