e07f29a561719ae3453c2667f3988eb67db17d82
corinnekrych <corinnekrych@gmail.com>
2013-04-03 02:48:30 +0200

    confess

--- web-app/js/threecircles/view/checkin-view.js
+++ web-app/js/threecircles/view/checkin-view.js
@@ -132,6 +132,9 @@ threecircles.view.checkinview = function (model, elements) {
     // user interface actions
     that.elements.list.on('pageinit', function (e) {
         that.listButtonClicked.notify();
+    });
+
+    that.elements.list.on('pageshow', function (e) {
         geolocationBackground.showMapBackground('map_canvas', {}) ;
     });
 
@@ -280,6 +283,10 @@ threecircles.view.checkinview = function (model, elements) {
 
     var resetForm = function (form) {
         $('#textarea-1').val('');
+
+        $('#div-for-upload').css('background-image', 'url("images/camera.png")');
+        $('#input-checkin-photo').attr('data-value', '');
+
         $('input[data-type="date"]').each(function() {
             $(this).scroller('destroy').scroller({
                 preset: 'date',
@@ -297,9 +304,6 @@ threecircles.view.checkinview = function (model, elements) {
             $.each(div.find('input:hidden'), function(id, input) {
                 if ($(input).attr('type') != 'file') {
                     $(input).val('');
-                } else {
-                    $(input).parent().css('background-image', 'url("images/camera.png")');
-                    $(input).attr('data-value', '');
                 }
             });
         }
